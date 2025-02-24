ef5c2dff42c1757117085e95306e2d4604b069a2
ivan.valchev <ivan.valchev.perez@gmail.com>
2025-02-24 07:24:18 +0000

    hyigiuo

--- babelfy/src/main/java/com/babel/babelfy/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.babel.babelfy.repository;
-
-import com.babel.babelfy.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- babelfy/src/main/java/com/babel/babelfy/repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.babel.babelfy.repository;
-
-import com.babel.babelfy.model.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-}
