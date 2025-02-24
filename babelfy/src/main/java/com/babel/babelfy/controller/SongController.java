50d8769ae891357f0effa1686ff6da8ebcfb1c9e
ivan.valchev <ivan.valchev.perez@gmail.com>
2025-02-24 08:59:14 +0000

    put y delete

--- babelfy/src/main/java/com/babel/babelfy/controller/SongController.java
+++ babelfy/src/main/java/com/babel/babelfy/controller/SongController.java
@@ -1,12 +1,15 @@
 package com.babel.babelfy.controller;
 
 import com.babel.babelfy.dto.SongDTO;
+import com.babel.babelfy.dto.SongDTORequest;
 import com.babel.babelfy.dto.SongDTORequestCreate;
+import com.babel.babelfy.dto.SongDTOResponse;
 import com.babel.babelfy.model.Category;
 import com.babel.babelfy.model.Song;
 import com.babel.babelfy.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +25,21 @@ public class SongController {
         return songService.getAll();
     }
   @PostMapping("")
-  public Song create (@RequestBody SongDTORequestCreate songDTO){
-    return songService.addSong(songDTO);
+  public Song create (@RequestBody SongDTORequestCreate songDTO) {
+      return songService.addSong(songDTO);
+
   }
+    @PutMapping("")
+    public ResponseEntity<SongDTOResponse> updateSong(@RequestBody SongDTORequest request) {
+        return ResponseEntity.ok(songService.updateSong(request));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteSong(@RequestBody SongDTORequest request) {
+        songService.deleteSong(request.getId()); // Solo se usa el ID
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
+
